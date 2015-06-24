b260f16f9c85b64b5355d39e96f6bc2cff570b85
soethan <soethan@gmail.com>
2015-06-24 17:05:04 +0800

    Initial Checkin

---
+++ BookingRequestsProcessor/Models/BookingRequest.cs
@@ -0,0 +1,48 @@
+﻿using FileHelpers;
+using System;
+using System.Collections.Generic;
+
+namespace BookingRequestsProcessor.Models
+{
+    [DelimitedRecord(",")]
+    [IgnoreFirst(1)]
+    [IgnoreLast(1)]
+    public class BookingRequest
+    {
+        public int SerialNo;
+        public string RequestNumber;
+        public string RequestorId;
+        public string RequestorFirstName;
+        public string RequestorLastName;
+        public string RequestorMobilePhoneNo;
+        public string RequestorDeskPhoneNo;
+        public string RequestorEmail;
+
+        public string PickUpAddress1;
+        public string PickUpAddress2;
+        public string PickUpAddressCity;
+        public string PickUpAddressProvince;
+        public string PickUpAddressPostal;
+        public string PickUpAddressCountry;
+        public decimal PickUpLatitue;
+        public decimal PickUpLongitute;
+
+        public string RecipientFirstName;
+        public string RecipientLastName;
+        public string RecipientMobilePhoneNo;
+        public string RecipientDeskPhoneNo;
+        public string RecipientEmail;
+
+        public string SendToAddress1;
+        public string SendToAddress2;
+        public string SendToAddressCity;
+        public string SendToAddressProvince;
+        public string SendToAddressPostal;
+        public string SendToAddressCountry;
+        public decimal SendToLatitue;
+        public decimal SendToLongitute;
+
+        public string Remarks { get; set; }
+
+    }
+}
---
+++ BookingRequestsProcessor/Program.cs
@@ -0,0 +1,116 @@
+﻿using BookingRequestsProcessor.Models;
+using FileHelpers;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookingRequestsProcessor
+{
+    class Program
+    {
+        const string FTP_REQUESTS = @"D:\Personal\TestPrjs\BookingRequestsProcessor\BookingRequestsProcessor\Data\FTP_Requests";
+        const string FTP_RESPONSE = @"D:\Personal\TestPrjs\BookingRequestsProcessor\BookingRequestsProcessor\Data\FTP_Response";
+        const string FTP_READ = @"D:\Personal\TestPrjs\BookingRequestsProcessor\BookingRequestsProcessor\Data\FTP_Read";
+        const string FTP_CORRUPTED = @"D:\Personal\TestPrjs\BookingRequestsProcessor\BookingRequestsProcessor\Data\FTP_Corrupted";
+
+        static void Main(string[] args)
+        {
+            ProcessBookingRequests();
+        }
+
+        static void ProcessBookingRequests()
+        {
+            
+            foreach (var fileName in Directory.GetFiles(FTP_REQUESTS, "*.csv"))
+            {
+                if (!IsFileCorrupted(fileName))
+                {
+                    ProcessFile(fileName);
+                }
+                else
+                {
+                    var requestFileName = Path.GetFileName(fileName);
+                    File.Move(fileName, string.Format("{0}\\{1}", FTP_CORRUPTED, requestFileName));
+                    File.Move(fileName.Replace(".csv", ".md5"), string.Format("{0}\\{1}", FTP_CORRUPTED, requestFileName.Replace(".csv", ".md5")));
+                }
+            }
+        }
+
+        private static bool IsFileCorrupted(string fileName)
+        {
+            var md5HashOfMD5 = File.ReadAllBytes(fileName.Replace(".csv", ".md5"));
+            var md5HashOfCsv = GetMD5Hash(fileName);
+            if (md5HashOfCsv.SequenceEqual(md5HashOfMD5))
+            {
+                return false;
+            }
+            return true;
+        }
+
+        static void ProcessFile(string fileName)
+        {
+            var engine = new FileHelperEngine(typeof(BookingRequest));
+
+            engine.ErrorManager.ErrorMode = ErrorMode.SaveAndContinue;
+
+            var bookingRequests = (BookingRequest[])engine.ReadFile(fileName);
+
+            if (engine.ErrorManager.Errors.Length > 0) {
+                ProcessErrorRecords(engine, bookingRequests.Length, fileName);
+            }
+
+            ProcessCorrectRecords(bookingRequests);
+            var requestFileName = Path.GetFileName(fileName);
+
+            File.Move(fileName, string.Format("{0}\\{1}", FTP_READ, requestFileName));
+            File.Move(fileName.Replace(".csv", ".md5"), string.Format("{0}\\{1}", FTP_READ, requestFileName.Replace(".csv", ".md5")));
+        }
+
+        private static void ProcessCorrectRecords(BookingRequest[] bookingRequests)
+        {
+            foreach (BookingRequest req in bookingRequests)
+            {
+                //TODO: Save record to BookingRequestsDB
+                Console.WriteLine(string.Format("SNo={0},RecipientFirstName={1},RecipientLastName={2}", req.SerialNo, req.RecipientFirstName, req.RecipientLastName));
+            }
+        }
+
+        private static void ProcessErrorRecords(FileHelperEngine engine, int numberOfGoodEntries, string fileName)
+        {
+            var sbErrors = new StringBuilder();
+            sbErrors.AppendLine(string.Format("Number of good entries: {0}", numberOfGoodEntries));
+            sbErrors.AppendLine(string.Format("Number of bad entries: {0}", engine.ErrorManager.Errors.Length));
+            sbErrors.AppendLine("List of bad entries:");
+            
+            foreach (var item in engine.ErrorManager.Errors)
+            {
+                sbErrors.AppendLine(item.RecordString);
+            }
+            sbErrors.Append("\"EOF\"");
+
+            var requestFileName = Path.GetFileName(fileName);
+            File.WriteAllText(string.Format("{0}\\{1}", FTP_RESPONSE, requestFileName.Replace("booking-request", "booking-response")), sbErrors.ToString());
+        }
+
+        private static byte[] GetMD5Hash(string fileName)
+        {
+            using (var md5 = MD5.Create())
+            {
+                using (var stream = File.OpenRead(fileName))
+                {
+                    return md5.ComputeHash(stream);
+                }
+            }
+        }
+
+        //private static void CreateMD5File(string fileName)
+        //{
+        //    var md5Hash = GetMD5Hash(fileName);
+        //    File.WriteAllBytes(fileName.Replace(".csv", ".md5"), md5Hash);
+        //}
+    }
+}
