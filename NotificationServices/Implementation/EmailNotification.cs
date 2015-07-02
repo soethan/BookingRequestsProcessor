53f8d69b072b5aca73882ce5bed0dd892e81af9b
Soe Than <soethan@gmail.com>
2015-07-02 22:50:31 +0800

    add EmailFolder setting

--- NotificationServices/Implementation/EmailNotification.cs
+++ NotificationServices/Implementation/EmailNotification.cs
@@ -1,6 +1,7 @@
 ﻿using NotificationServices.Interfaces;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -12,7 +13,7 @@ namespace NotificationServices.Implementation
     {
         public void Send(string sender, List<string> receiver, string subject, string content)
         {
-            File.WriteAllText(@"D:\TestPrjs\BookingRequestsProcessor\" + subject + "-" + DateTime.Now.ToString("yyyy-MM-dd-hh-mm-ss") + ".txt", content);
+            File.WriteAllText(ConfigurationManager.AppSettings["EmailFolder"] + subject + "-" + DateTime.Now.ToString("yyyy-MM-dd-hh-mm-ss") + ".txt", content);
         }
     }
 }
