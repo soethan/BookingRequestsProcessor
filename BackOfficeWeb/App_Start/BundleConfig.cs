68a17ace1366869aee55bd0c2da6ec0b25220409
soethan <soethan@gmail.com>
2015-07-03 18:05:00 +0800

    dropdown with bootstrap select

--- BackOfficeWeb/App_Start/BundleConfig.cs
+++ BackOfficeWeb/App_Start/BundleConfig.cs
@@ -21,11 +21,13 @@ namespace BackOfficeWeb
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+                      "~/Scripts/respond.js",
+                      "~/Scripts/bootstrap-select.min.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/bootstrap-select.min.css"));
         }
     }
 }
